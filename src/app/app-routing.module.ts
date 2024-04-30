3fdc5cc4ff7a0e00ee4e2e2c892a6b661b23ead7
DavitMamardashvili <mamardashvilidavit7@gmail.com>
2024-04-30 11:26:38 +0400

    Implemented version 1 of the home page and added the 404 error page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
-  
+  { path: '', redirectTo: 'home', pathMatch: 'full' }, // Redirects empty path to 'home'
+  { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) }, // Lazy loads the Home module when '/home' is accessed
 
+  { path: '**', redirectTo: 'not-found' }, // Redirects any unknown routes to 'not-found'
+  { path: 'not-found', loadChildren: () => import('./pages/not-found/not-found.module').then(m => m.NotFoundModule) }, // Lazy loads the NotFound module when '/not-found' is accessed
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
