567b3805db4b5e38cab752ed67331e2f311f0c98
DavitMamardashvili <mamardashvilidavit7@gmail.com>
2024-05-08 13:37:45 +0400

    Fixed all bugs

--- src/app/pages/hotelinfo/hotelinfo/hotelinfo.component.ts
+++ src/app/pages/hotelinfo/hotelinfo/hotelinfo.component.ts
@@ -10,14 +10,17 @@ import { Location } from '@angular/common';
 export class HotelinfoComponent {
   hotel: any;
 
-  constructor(private httpService: HttpService, private location: Location) { 
-    const currentUrl = this.location.path();
-    const id = currentUrl[currentUrl.length - 1];
-    this.httpService.getData('get-by-id?id=' + id).subscribe((response: any) => {
+  constructor(private httpService: HttpService, private location: Location) {
+    this.httpService.getData('get-by-id?id=' + this.getID()).subscribe((response: any) => {
       this.hotel = response;
       if (this.hotel.images.length < 8) {
         this.httpService.fetchUnsplashImages(this.hotel, 8 - this.hotel.images.length).subscribe();
       }
     })
   }
+
+  getID():string {
+    return this.location.path().includes("3D") 
+      ? this.location.path().split("3D")[1] 
+      : '';  }
 }
